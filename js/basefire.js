4944641ef3af24974c3c2316272377f42521b8ec
jcgter777 :P <34496783+jcgter777@users.noreply.github.com>
2019-12-07 17:58:19 -0500

    Create basefire.js

---
+++ js/basefire.js
@@ -0,0 +1,35 @@
+var config = {
+	apiKey: "AIzaSyBlcxJyIQ5YfkL9-pgnJpoIOvgrCYAu4_E",
+	authDomain: "make-school-great-again.firebaseapp.com",
+	databaseURL: "https://make-school-great-again.firebaseio.com",
+	projectId: "make-school-great-again",
+	storageBucket: "",
+	messagingSenderId: "643533486952"
+};
+firebase.initializeApp(config);
+
+let auth = firebase.auth();
+auth.onAuthStateChanged(user => {
+	if (user) {
+		signedIn();
+	}
+	else {
+		signedOut();
+	}
+});
+
+
+function signedIn() {
+	document.getElementById('googlesignin').classList = 'hidden';
+	document.getElementById('signinsuccess').classList = 'visible';
+	document.getElementById("truediv").classList = 'hidden';
+}
+
+function signedOut() {
+	/* alert("Sign in with Google!");*/
+}
+
+document.getElementById('signin-div').addEventListener('click', () => {
+	var provider = new firebase.auth.GoogleAuthProvider();
+	firebase.auth().signInWithPopup(provider).then(function() {});
+});
